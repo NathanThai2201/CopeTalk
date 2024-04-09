b750d99113d21ce1c6c446acb268c2b0fd8b576d
Nathan Thai <108090182+NathanThai2201@users.noreply.github.com>
2024-04-08 22:32:21 -0600

    Add files via upload

--- CopeTalk.py
+++ CopeTalk.py
@@ -1,41 +1,29 @@
 import random
-
 ascii_low = 32
 ascii_high = 126
-
-def encrypt(landing_input):
-    input2 = double_caesar(landing_input,0)
-    input2.reverse()
-    input2 = fibonacci_swap(input2)
-    input2 = euler_estimate_insert(input2)
-    input2 = no_cap(input2,0)
-    input2 = palindrome_swap(input2)
-    input2 = vigenere(input2,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-    input2.reverse()
-    input2 = vigenere(input2,'Coperime was here!')
-    input2.reverse()
-    input2 = double_caesar(input2,1)
-    input2 = pi_estimate_insert(input2)
-    input2 = zalgo(input2)
-    return input2
-
 def decrypt(landing_input):
-    input2 = reverse_zalgo(landing_input)
-    input2 = pi_estimate_pop(input2)
-    input2 = double_caesar(input2,0)
+    input2 = p(landing_input)
+    input2 = d(input2,0)
     input2.reverse()
-    input2 = vigenere_reverse(input2,'Coperime was here!')
+    input2 = v(input2,g(120,seed=3))
+    input2 = d(input2,1)
     input2.reverse()
-    input2 = vigenere_reverse(input2,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-    input2 = palindrome_swap(input2)
-    input2 = no_cap(input2,1)
-    input2 = euler_estimate_pop(input2)
-    input2 = fibonacci_swap(input2)
+    input2 = v(input2,g(500,seed=5))
+    input2 = v(input2,g(441,seed=120))
+    input2 = v(input2,g(441,seed=125))
+    input2 = v(input2,g(128,seed=10))
+    input2 = v(input2,g(300,seed=129))
+    input2 = ps(input2)
+    input2 = nc(input2,1)
+    input2 = d(input2,0)
+    input2 = e(input2)
+    input2 = f(input2)
     input2.reverse()
-    input2 = double_caesar(input2,1)
-    return input2
+    input2 = r(input2)
+    input2 = d(input2,1)
 
-def double_caesar(t_input,index):
+    return input2
+def d(t_input,index):
     for i in range(len(t_input)):
         if index == 0:
             temp = ord(t_input[i])-3
@@ -49,7 +37,7 @@ def double_caesar(t_input,index):
             temp -= (ascii_high-ascii_low)+1
         t_input[i]=chr(temp)
     return t_input
-def fibonacci_swap(t_input):
+def f(t_input):
     fib = [0,1]
     i=2
     while fib[i-1]<len(t_input):
@@ -64,17 +52,7 @@ def fibonacci_swap(t_input):
             t_input[index1] = t_input[index2]
             t_input[index2] = temp
     return t_input
-def euler_estimate_insert(t_input):
-    e = list(str(int(((1+1/10000)**10000)*10**18)))
-    f = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    j=0
-    for i in range(len(e)):
-        j+=int(e[i])
-        if j>len(t_input): break
-        index = random.randint(0, len(f) - 1)
-        t_input.insert(j,f[index])
-    return t_input
-def euler_estimate_pop(t_input):
+def e(t_input):
     e = list(str(int(((1+1/10000)**10000)*10**18)))
     j=0
     for i in range(len(e)):
@@ -83,9 +61,9 @@ def euler_estimate_pop(t_input):
         t_input.pop(j)
         j-=1
     return t_input
-def no_cap(t_input,index):
+def nc(t_input,index):
     amount=5
-
+    random.seed()
     if index == 1:
         return t_input[amount:len(t_input)-amount]
     if index == 0:
@@ -96,7 +74,7 @@ def no_cap(t_input,index):
             index2 = random.randint(0, len(f) - 1)
             t_input.append(f[index2])
         return t_input
-def palindrome_swap(t_input):
+def ps(t_input):
     i = 0
     j = len(t_input)-1
     while i<j:
@@ -106,15 +84,7 @@ def palindrome_swap(t_input):
         i+=2
         j-=2
     return t_input
-def vigenere(t_input,key):
-    out = []
-    key_length = len(key)
-    for i in range(len(t_input)):
-        shift = ord(key[i % key_length]) - 32
-        encrypted_char = chr((ord(t_input[i]) - 32 + shift) % 95 + 32)
-        out.append(encrypted_char)
-    return out
-def vigenere_reverse(t_input,key):
+def v(t_input,key):
     out = []
     key_length = len(key)
     for i in range(len(t_input)):
@@ -122,18 +92,8 @@ def vigenere_reverse(t_input,key):
         decrypted_char = chr((ord(t_input[i]) - 32 - shift) % 95 + 32)
         out.append(decrypted_char)
     return out
-def pi_estimate_insert(t_input):
-    pi=list(str(31415926535897932384626433832795028841971))
-    f = f = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    j=0
-    for i in range(len(pi)):
-        j+=int(pi[i])
-        if j>len(t_input): break
-        index = random.randint(0, len(f) - 1)
-        t_input.insert(j,f[index])
-    return t_input
-def pi_estimate_pop(t_input):
-    pi=list(str(31415926535897932384626433832795028841971))
+def p(t_input):
+    pi=list(str(31415926535897932384626433832795288419716939937515))
     j=0
     for i in range(len(pi)):
         j+=int(pi[i])
@@ -141,40 +101,28 @@ def pi_estimate_pop(t_input):
         t_input.pop(j)
         j-=1
     return t_input
-def zalgo(t_input):
-    marks = list(map(chr, range(768, 879)))
-    string = ''.join(map(str,t_input))
-    words = string.split()
-    final = ' '.join(''.join(c + ''.join(random.choice(marks) for _ in range(i // 2 + 1)) * c.isalnum() for c in word) for i, word in enumerate(words))
-    final = [i for i in final]
-    return final
-def reverse_zalgo(t_input):
-    string = ''.join(map(str,t_input))
-    words = string.split()
-    reconstructed_words = []
-
-    for word in words:
-        reconstructed_word = ""
-        i = 0
-        while i < len(word):
-            reconstructed_word += word[i]
-            i += 1
-            while i < len(word) and ord(word[i]) >= 768 and ord(word[i]) <= 879:
-                i += 1
-        reconstructed_words.append(reconstructed_word)
-    final = ' '.join(reconstructed_words)
-    final = [i for i in final]
-    return final
+def r(t_input):
+    pad_const=9
+    if t_input[0].isdigit():
+        length = int(t_input[0])
+        if length <= pad_const:
+            for i in range(pad_const - length):
+                t_input.pop(0)
+            t_input.pop(0)
+    else:
+        t_input.pop(0)
+    return t_input
+def g(length, seed=None):
+    if seed is not None:
+        random.seed(seed)
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    random_string = ''.join(random.choice(characters) for _ in range(length)) 
+    return random_string
 def main():
-    landing_input = input('Input:')
+    with open("in.txt", 'r') as file:
+        landing_input = file.read()
     t_input = [i for i in landing_input]
-    mode = input("Encrypt or Decrypt? [E/D]").lower()
-    if mode == "e":
-        result = encrypt(t_input)
-    if mode == "d":
-        result = decrypt(t_input)
+    result = decrypt(t_input)
     final = ''.join(map(str, result))
     print(final)
-    return 0
-
 main()
